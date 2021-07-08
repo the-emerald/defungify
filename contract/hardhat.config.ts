9de33dd6dcfa1946f46a92de09fd0055fe9cc259
Anson <git@anson-cheung.com>
2021-07-08 08:06:21 +0800

    Add gas reporter

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter"
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,5 +16,17 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 export default {
-  solidity: "0.8.0",
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 1
+  }
 };
