8c14e02b20c7a440eaeedb0fbb3c20bd1bb89d43
Anson <git@anson-cheung.com>
2021-07-12 07:31:26 +0800

    Update for deployment

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
     return(
         <div>
             <h1>Defungify</h1>
-            <p>Wrap your ERC-20 tokens into non-fungible packets, alongside a customised message.</p>
+            <p>Wrap your ERC-20 tokens into non-fungible packets, along with a custom message.</p>
             <p><b><i>Using this smart contract carries inherent risk. Don't put your life savings into me!</i></b></p>
             <p><b>Available on</b>: polygon, xdai, rinkeby. Mainnet coming soon!</p>
         </div>
