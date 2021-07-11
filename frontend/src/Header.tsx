0b78b0f0866b1aba463c42da221cf9023794b6d2
Anson <git@anson-cheung.com>
2021-07-11 22:46:48 +0800

    Add warning for smart contract risks

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -2,7 +2,8 @@ export function Header() {
     return(
         <div>
             <h1>Defungify</h1>
-            <p>Wrap your ERC-20 tokens into non-fungible packets, alongside a customised message!</p>
+            <p>Wrap your ERC-20 tokens into non-fungible packets, alongside a customised message.</p>
+            <p><b><i>Using this smart contract carries inherent risk. Don't put your life savings into me!</i></b></p>
             <p><b>Available on</b>: rinkeby</p>
         </div>
     )
