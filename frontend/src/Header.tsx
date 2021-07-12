75144a2b72a66de46525322bf682b4901bcfa398
Anson <git@anson-cheung.com>
2021-07-12 08:14:18 +0800

    Add source code link

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,9 +1,18 @@
 export function Header() {
-    return(
+    return (
         <div>
             <h1>Defungify</h1>
             <p>Wrap your ERC-20 tokens into non-fungible packets, along with a custom message.</p>
-            <p><b><i>Using this smart contract carries inherent risk. Don't put your life savings into me!</i></b></p>
+            <p>
+                <b>
+                    <i>Using this smart contract carries inherent risk. Don't put your life savings into me!
+                    </i>
+                </b>
+                &nbsp;&nbsp;
+                <a href="https://github.com/the-emerald/defungify">
+                    Source code available here.
+                </a>
+            </p>
             <p><b>Available on</b>: polygon, xdai, rinkeby. Mainnet coming soon!</p>
         </div>
     )
